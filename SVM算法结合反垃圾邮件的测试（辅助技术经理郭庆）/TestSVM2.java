3e55d196efe0b06c69cd62230d9aa6a25621d800
Night-Shine <43108892+Night-Shine@users.noreply.github.com>
2019-07-02 19:05:48 +0800

    刘一泽-2019.07.02
    SVM算法结合反垃圾邮件的测试（辅助技术经理郭庆）

---
+++ SVM算法结合反垃圾邮件的测试（辅助技术经理郭庆）/TestSVM2.java
@@ -0,0 +1,36 @@
+package mysystem;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class TestSVM2 {
+	public static void main(String args[]) {
+		//ÎªSVMËã·¨×ö´Êµä×¼±¸<´Ê-ID>
+//		int i=1;
+//		HashMap<String,Integer> hm= new HashMap();
+//		Dictionary dic = new Dictionary("D:\\ÓÊ¼þ\\´Êµä.txt");
+//		for (String key : dic.hm.keySet()) {
+//			hm.put(key, i);
+//			i++;
+//		}
+//		String line = System.getProperty("line.separator");
+//    	StringBuffer str = new StringBuffer();
+//    	File file = new File("D:\\´Êµä.txt");
+//		if (file.exists()){
+//			file.delete();
+//		}
+//    	FileWriter fw;
+//    	try {
+//    		fw = new FileWriter("D:\\´Êµä.txt", true);
+//        	for(String key : hm.keySet()) {
+//        		str.append(key+" : "+hm.get(key)).append(line);
+//        }
+//        	fw.write(str.toString());
+//    		fw.close();
+//	} catch (IOException e) {
+//		e.printStackTrace();
+//	}
+	}
+}
